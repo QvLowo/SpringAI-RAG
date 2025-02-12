88b9cf08a8a96acd9aaac6302198caed4134ebe7
QuL <qvlywk@gmail.com>
2025-02-12 15:47:21 +0800

    新增註解，更新流式為打字機輸出。

--- src/main/java/com/qvl/springairag/controller/ChatController.java
+++ src/main/java/com/qvl/springairag/controller/ChatController.java
@@ -1,21 +1,20 @@
 package com.qvl.springairag.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
 
     private final ChatClient chatClient;
 
-    // 建構子注入
-    ChatController(ChatClient chatClient) {
-        this.chatClient = chatClient;
-    }
-
+    // 全部跑好才回應
     @GetMapping("/chat")
     public String chat(@RequestParam String prompt) {
         return this.chatClient.prompt()
@@ -24,7 +23,8 @@ public class ChatController {
                 .content();
     }
 
-    @GetMapping("/fluxChat")
+    // 流氏回應，邊想邊回，像打字機輸出
+    @GetMapping(value="/fluxChat",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> fluxChat(@RequestParam String prompt) {
         return this.chatClient.prompt()
                 .user(prompt)
