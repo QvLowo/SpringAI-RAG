e9bf8bf62318e6b7fdbf6b309db45c42449ca88e
QuL <qvlywk@gmail.com>
2025-02-12 20:40:02 +0800

    新增功能: AI新增記憶功能

--- src/main/java/com/qvl/springairag/config/AIConfig.java
+++ src/main/java/com/qvl/springairag/config/AIConfig.java
@@ -1,6 +1,9 @@
 package com.qvl.springairag.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +16,14 @@ public class AIConfig {
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
 //        你是資深的股票分析大師，擁有超過30年的投資經驗，擅長技術分析、基本面分析以及市場趨勢預測
-        return builder.defaultSystem("You are a seasoned stock analysis master with over 30 years of investment experience, specializing in technical analysis, fundamental analysis, and market trend forecasting. response always use zh-tw")
+        return builder
+                .defaultSystem("""
+                        You are a seasoned stock analysis master with over 30 years of investment experience,
+                         specializing in technical analysis, fundamental analysis, and market trend forecasting.
+                        """)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory()),
+                        new SimpleLoggerAdvisor())
                 .build();
     }
 
--- src/main/java/com/qvl/springairag/controller/ChatController.java
+++ src/main/java/com/qvl/springairag/controller/ChatController.java
@@ -2,11 +2,15 @@ package com.qvl.springairag.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,8 +29,13 @@ public class ChatController {
 
     // 流氏回應，邊想邊回，像打字機輸出
     @GetMapping(value="/fluxChat",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> fluxChat(@RequestParam String prompt) {
+    public Flux<String> fluxChat(@RequestParam String chatId, @RequestParam String prompt) {
         return this.chatClient.prompt()
+                // 讓ai有記憶
+                .advisors(advisor -> advisor.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
+                // 紀錄token數
+                .advisors(new SimpleLoggerAdvisor())
                 .user(prompt)
                 .stream().content();
     }
